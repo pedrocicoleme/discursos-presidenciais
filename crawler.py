33809c8ec2ff5708e1285c71106781b567ac67d1
Pedro <pedrocicoleme@gmail.com>
2016-03-06 00:38:37 -0300

    conferindo links

--- crawler.py
+++ crawler.py
@@ -7,6 +7,7 @@ logger = logging.getLogger()
 import requests
 import urlparse
 import lxml.html
+import regex
 
 def html_viewer(html, block=False):
     import uuid
@@ -41,8 +42,14 @@ def extract_discursos():
 
         root = lxml.html.fromstring(r.content)
 
-        all_links = [[x.get(u'title'), urlparse.urljoin(presidente_link, x.get(u'href'))] for x in \
-                root.xpath(u'//div[@id="content"]/div[@id="parent-fieldname-text"]/ul[1]/li/a')]
+        all_links = dict()
+
+        for link in root.xpath(u'//div[@id="content"]/div[@id="parent-fieldname-text"]/ul[1]/li/a'):
+            titulo = link.get(u'title')
+            real_link = link.get(u'href')
+
+            if not regex.search(ur'^(Biografia|Foto|Ministérios|Vice-presidente|Viage|Órgã|Discursos Vice-Presidente|Substituto|Nereu)', titulo, regex.VERSION1|regex.IGNORECASE):
+                all_links[titulo] = urlparse.urljoin(presidente_link, real_link)
 
         pronunciamento_link = root.xpath(u'//a[@title="pronunciamento"]/@href')
         discursos_link = root.xpath(u'//a[@title="Discursos"]/@href')
@@ -50,7 +57,7 @@ def extract_discursos():
 
         presidentes.append({
             u'nome': root.xpath(u'//h1[@class="documentFirstHeading"]/span/text()')[0].strip(),
-            u'presidente_link': presidente_link,
+            u'url': presidente_link,
             u'links': all_links})
 
     # for idx, presidente in enumerate(presidentes):
@@ -61,8 +68,10 @@ def extract_discursos():
 
     #         presidentes[idx][u'mensagens_links'] = root.xpath(u'//span[@class="contenttype-file summary"]/a/@href')
 
-    from pprint import pprint
-    pprint(presidentes)
+    for presidente in presidentes:
+        if len(presidente[u'links']):
+            from pprint import pprint
+            pprint(presidente)
 
     return
 
