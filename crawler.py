745c2a3469660e8badc6a60554105eba40f67b3a
Pedro <pedrocicoleme@gmail.com>
2016-03-06 00:19:18 -0300

    pegando todos os links

--- crawler.py
+++ crawler.py
@@ -11,7 +11,7 @@ import lxml.html
 def html_viewer(html, block=False):
     import uuid
     filename = u'/tmp/%s.html' % uuid.uuid4()
-    
+
     with open(filename, 'w') as f:
         f.write(html)
 
@@ -37,29 +37,29 @@ def extract_discursos():
     for presidente_link in presidentes_links:
         r = requests.get(presidente_link)
 
-        html_viewer(r.content)
+        #html_viewer(r.content)
 
         root = lxml.html.fromstring(r.content)
 
-        nome = root.xpath(u'//h1[@class="documentFirstHeading"]/span/text()')[0].strip()
+        all_links = [[x.get(u'title'), urlparse.urljoin(presidente_link, x.get(u'href'))] for x in \
+                root.xpath(u'//div[@id="content"]/div[@id="parent-fieldname-text"]/ul[1]/li/a')]
+
         pronunciamento_link = root.xpath(u'//a[@title="pronunciamento"]/@href')
         discursos_link = root.xpath(u'//a[@title="Discursos"]/@href')
         mensagens_link = root.xpath(u'//a[contains(@title, "Mensagens")]/@href')
 
         presidentes.append({
-            u'nome': nome,
+            u'nome': root.xpath(u'//h1[@class="documentFirstHeading"]/span/text()')[0].strip(),
             u'presidente_link': presidente_link,
-            u'pronunciamento_link': urlparse.urljoin(presidente_link, pronunciamento_link[0]) if len(pronunciamento_link) else None,
-            u'discursos_link': urlparse.urljoin(presidente_link, discursos_link[0]) if len(discursos_link) else None,
-            u'mensagens_link': urlparse.urljoin(presidente_link, mensagens_link[0]) if len(mensagens_link) else None})
+            u'links': all_links})
 
-    for idx, presidente in enumerate(presidentes):
-        if presidente[u'mensagens_link'] != None:
-            r = requests.get(presidente[u'mensagens_link'])
+    # for idx, presidente in enumerate(presidentes):
+    #     if presidente[u'mensagens_link'] != None:
+    #         r = requests.get(presidente[u'mensagens_link'])
 
-            root = lxml.html.fromstring(r.content)
+    #         root = lxml.html.fromstring(r.content)
 
-            presidentes[idx][u'mensagens_links'] = root.xpath(u'//span[@class="contenttype-file summary"]/a/@href')
+    #         presidentes[idx][u'mensagens_links'] = root.xpath(u'//span[@class="contenttype-file summary"]/a/@href')
 
     from pprint import pprint
     pprint(presidentes)
